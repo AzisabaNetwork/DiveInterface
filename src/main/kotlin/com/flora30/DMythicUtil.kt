036be7a9806da297898934b62450c9d0a3fe257e
ふろーら <73652094+Flora300@users.noreply.github.com>
2023-04-22 23:57:33 +0900

    util移動

---
+++ src/main/kotlin/com/flora30/DMythicUtil.kt
@@ -0,0 +1,19 @@
+package com.flora30
+
+import io.lumine.xikage.mythicmobs.MythicMobs
+import io.lumine.xikage.mythicmobs.api.exceptions.InvalidMobTypeException
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.Location
+import org.bukkit.entity.Entity
+
+object DMythicUtil {
+    fun spawnMob(mobName: String, location: Location): Entity? {
+        return try {
+            MythicMobs.inst().apiHelper.spawnMythicMob(mobName, location)
+        } catch (e: InvalidMobTypeException){
+            Bukkit.getLogger().info("" + ChatColor.YELLOW + "[DiveCore-Mythic] Mob名 " + mobName + " が見つかりません")
+            null
+        }
+    }
+}
