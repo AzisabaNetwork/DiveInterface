70836c72fe8027cdcc11739c6a2fe69ecb12cd5a
ふろーら <73652094+Flora300@users.noreply.github.com>
2023-05-18 01:08:58 +0900

    ヘルプイベント

---
+++ src/main/kotlin/com/flora30/diveapin/event/HelpEvent.kt
@@ -0,0 +1,16 @@
+package com.flora30.diveapin.event
+
+import org.bukkit.entity.Player
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+class HelpEvent (
+    val player: Player,
+    val type: HelpType
+    ): Event() {
+
+    private val handlerList = HandlerList()
+    override fun getHandlers(): HandlerList {
+        return handlerList
+    }
+}
---
+++ src/main/kotlin/com/flora30/diveapin/event/HelpType.kt
@@ -0,0 +1,20 @@
+package com.flora30.diveapin.event
+
+enum class HelpType {
+    HelpGUI,
+    MenuGUI,
+    StatusGUI,
+    CraftListGUI,
+    UseStamina,
+    LootChestGUI,
+    CurseGUI,
+    Gather,
+    FastTravelGUI,
+    ReturnAble,
+    WhistleGUI,
+    BaseGUI,
+    Base_Cook,
+    BaseGUI_PowerUp,
+    Trade,
+    Rope
+}
