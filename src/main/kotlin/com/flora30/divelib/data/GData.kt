84ceff0ab7caff81fd3720441d650741626369bb
ふろーら <73652094+Flora300@users.noreply.github.com>
2023-05-30 17:15:33 +0900

    Gimmick仮組み

---
+++ src/main/kotlin/com/flora30/divelib/data/GData.kt
@@ -0,0 +1,11 @@
+package com.flora30.divelib.data
+
+import org.bukkit.Location
+import org.bukkit.entity.Player
+
+// ギミック用に取得するデータ
+class GData(
+    val player: Player,
+    val location: Location
+) {
+}
