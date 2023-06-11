5393255c8cc61aac1e0c926e16cc3d03e05832b9
ふろーら <73652094+Flora300@users.noreply.github.com>
2023-06-11 16:06:15 +0900

    ギミックデータ追加

--- src/main/kotlin/com/flora30/divelib/data/gimmick/Gimmick.kt
+++ src/main/kotlin/com/flora30/divelib/data/gimmick/Gimmick.kt
@@ -8,6 +8,7 @@ class Gimmick(
     val actions: ArrayList<GAction>,
     val spawnRate: Int,
     val group: String,
-    val random: Double // 0 ~ 1
+    val random: Double, // 0 ~ 1
+    val logTime: Int
 ) {
 }
