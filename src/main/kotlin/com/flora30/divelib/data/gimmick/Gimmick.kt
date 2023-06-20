49106f538da306686659fd7056a2da49e68a59b6
ふろーら <73652094+Flora300@users.noreply.github.com>
2023-06-21 08:19:28 +0900

    ログ削除条件の距離を追加

--- src/main/kotlin/com/flora30/divelib/data/gimmick/Gimmick.kt
+++ src/main/kotlin/com/flora30/divelib/data/gimmick/Gimmick.kt
@@ -9,7 +9,8 @@ class Gimmick(
     val spawnRate: Int,
     val group: String?,
     val random: Double, // 0 ~ 1
-    val logTime: Int
+    val logTime: Int,
+    val removeRange: Int, // ログを消去する距離
 ) {
 
     // グループ対応
