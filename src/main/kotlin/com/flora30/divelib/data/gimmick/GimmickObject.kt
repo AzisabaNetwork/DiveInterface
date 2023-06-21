26af1ef697bf5264928f42e598404700f8cc9178
ふろーら <73652094+Flora300@users.noreply.github.com>
2023-06-21 09:40:39 +0900

    ギミックID文字数省略用の関数を追加

--- src/main/kotlin/com/flora30/divelib/data/gimmick/GimmickObject.kt
+++ src/main/kotlin/com/flora30/divelib/data/gimmick/GimmickObject.kt
@@ -6,4 +6,27 @@ object GimmickObject {
 
     // layerID | ギミックログ
     val layerLogMap = hashMapOf<String,HashSet<GimmickLog>>()
+
+
+    /**
+     * データベース保存時の文字数省略用
+     * データベース保存する予定のギミックだけ登録
+     */
+    fun getIntID(strID: String): Int{
+        return when(strID){
+            "Chest_Grass" -> 0
+            "Chest_Ruin" -> 1
+            "FallRock" -> 2
+            else -> -1
+        }
+    }
+
+    fun getStringID(intID: Int): String?{
+        return when(intID){
+            0 -> "Chest_Grass"
+            1 -> "Chest_Ruin"
+            2 -> "FallRock"
+            else -> null
+        }
+    }
 }
